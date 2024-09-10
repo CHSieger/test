cafcfaa37385f331f170d261fbe15e426dd22807
CHSieger <privat.christiansieger@gmail.com>
2024-09-10 10:11:24 +0200

    Updated by CrossUI No-Code App Builder

--- App/js/index.js
+++ App/js/index.js
@@ -19,7 +19,50 @@ xui.Class('App', 'xui.Module',{
         iniComponents : function(){
             // [[Code created by CrossUI RAD Studio
             var host=this, children=[], append=function(child){children.push(child.get(0));};
-
+            
+            append(
+                xui.create("xui.UI.Button")
+                .setHost(host,"knopf")
+                .setName("btn")
+                .setLeft("15.238095238095237em")
+                .setTop("25.142857142857142em")
+                .setWidth("12.114285714285714em")
+                .setHeight("5.866666666666666em")
+                .setCaption("Button")
+                .onClick([
+                    {
+                        "desc" : "Action 1",
+                        "type" : "control",
+                        "target" : "textfeld",
+                        "args" : [
+                            "{page.textfeld.setUIValue()}",
+                            undefined,
+                            undefined,
+                            "hallo test"
+                        ],
+                        "method" : "setUIValue",
+                        "redirection" : "other:callback:call",
+                        "event" : 1
+                    },
+                    "_knopf_onclick"
+                ])
+            );
+            
+            append(
+                xui.create("xui.UI.Input")
+                .setHost(host,"textfeld")
+                .setName("textfeld")
+                .setDataField("hallo")
+                .setReadonly(true)
+                .setLeft("28.19047619047619em")
+                .setTop("9.142857142857142em")
+                .setWidth("18em")
+                .setHeight("10em")
+                .setLabelSize("8em")
+                .setLabelCaption("Text Area")
+                .setMultiLines(true)
+            );
+            
             return children;
             // ]]Code created by CrossUI RAD Studio
         },
@@ -28,6 +71,17 @@ xui.Class('App', 'xui.Module',{
         customAppend : function(parent, subId, left, top){
             // "return false" will cause all the internal UI controls will be added to the parent panel
             return false;
+        },
+        /**
+         * Fired when user click it
+         * @method onClick [xui.UI.Button event]
+         * @param {xui.UIProfile.} profile  The current control's profile object
+         * @param {Event} e , Dom event object
+         * @param {Element.xui} src  id or Dom Element
+         * @param {} value  Object
+        */
+        _knopf_onclick:function(profile, e, src, value){
+            var ns = this, uictrl = profile.boxing();
         }
         /*,
         // To determine how properties affects this module
