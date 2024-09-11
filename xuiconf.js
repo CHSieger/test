79afd4f6c6dfdd45e4e54933e79580b84e9135ee
CHSieger <privat.christiansieger@gmail.com>
2024-09-11 13:38:23 +0200

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -37,64 +37,63 @@ xui.ini.$DefaultProp={
 
 // [[Global Functions
 xui.$cache.functions = {
-    "$APICaller:beforeData":{
-        "desc":"exception handler",
-        "params":[
+    "$APICaller:beforeData" : {
+        "desc" : "exception handler",
+        "params" : [
             {
-                "id":"rspData",
-                "type":"Hash",
-                "desc":""
+                "id" : "rspData",
+                "type" : "Hash",
+                "desc" : ""
             }
         ],
-        "actions":[
+        "actions" : [
             {
-                "desc":"if returns exception",
-                "type":"other",
-                "target":"msg",
-                "args":[
+                "desc" : "if returns exception",
+                "type" : "other",
+                "target" : "msg",
+                "args" : [
                     "API returns exception",
                     "{args[0].error.message}"
                 ],
-                "method":"alert",
-                "conditions":[
+                "method" : "alert",
+                "conditions" : [
                     {
-                        "left":"{args[0].error}",
-                        "symbol":"non-empty",
-                        "right":""
+                        "left" : "{args[0].error}",
+                        "symbol" : "non-empty",
+                        "right" : ""
                     }
                 ],
-                "timeout":0,
-                "onOK":2
+                "timeout" : 0,
+                "onOK" : 2
             }
         ]
     },
-    "$APICaller:onError":{
-        "desc":"error handler",
-        "params":[
+    "$APICaller:onError" : {
+        "desc" : "error handler",
+        "params" : [
             {
-                "id":"rspData",
-                "type":"Hash",
-                "desc":""
+                "id" : "rspData",
+                "type" : "Hash",
+                "desc" : ""
             }
         ],
-        "actions":[
+        "actions" : [
             {
-                "desc":"if API raises error",
-                "type":"other",
-                "target":"msg",
-                "args":[
+                "desc" : "if API raises error",
+                "type" : "other",
+                "target" : "msg",
+                "args" : [
                     "API raises error",
                     "{args[0]}"
                 ],
-                "method":"alert",
-                "onOK":2,
-                "timeout":0
+                "method" : "alert",
+                "onOK" : 2,
+                "timeout" : 0
             }
         ]
     }
 };
 // ]]Global Functions
-
 // [[Global Data
 xui.$cache.data = {
     "$DATE_FORMAT":"gmt"
